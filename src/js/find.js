33322831c4267a4f99311c644364ffdbf71b58cd
kzkxdzt <ljlbxy@126.com>
2017-01-31 11:54:06 +0800

    update

--- src/js/find.js
+++ src/js/find.js
@@ -11,6 +11,10 @@ $(() => {
     }
     Find.prototype = Object.create(Scrolladdcomponents.prototype);
     Find.constructor = Find;
+    Find.prototype.render = function(){
+        this.showData();
+        this.skipEvent();
+    };
     Find.prototype.getTemplate = function (item) {
         return "<div class='search_item'><a href='moviedetails.html?id="+item._id+"'><img src="+item.images.large+" alt=''><span class='font-h'>"+item.title+"</span></a></div>";
     };
@@ -30,18 +34,14 @@ $(() => {
             $(html).appendTo($(".swiper-wrapper"));
         }
     };
-    Find.prototype.bindEvent = function () {
-        this.skip();
-    };
-    Find.prototype.skip = function () {
-        //点击搜索跳转搜索Ajax.load
+    Find.prototype.skipEvent = function () {
         $("#search").on("touchend", function() {
             let searchValue = $(".search_item").eq(0).find("span").html() || "";
             window.location.href = `search.html?name=${encodeURIComponent(searchValue)}`;
         });
     };
     const search = new Find({
-        id: "find",
+        id: "find_scroll",
         hasDel: false
     });
     search.init();
