97cf7f3c1a7b633b8ba53c11c45c165ce12373ce
kzkxdzt <ljlbxy@126.com>
2017-01-31 18:43:16 +0800

    update

--- src/js/find.js
+++ src/js/find.js
@@ -5,37 +5,39 @@ const Scrolladdcomponents = require("./components/scrolladdcomponents");
 $(() => {
     function Find(opt){ 
         Scrolladdcomponents.call(this, opt);
-        this.page = 1;
         this.holdPosition = 0;
         this.mySwiper = "";
     }
     Find.prototype = Object.create(Scrolladdcomponents.prototype);
     Find.constructor = Find;
     Find.prototype.render = function(){
-        this.showData();
         this.skipEvent();
+        this.getTemplate();
     };
-    Find.prototype.getTemplate = function (item) {
-        if (!item) {
-            return null;
-        }
-        return "<div class='search_item'><a href='moviedetails.html?id="+item._id+"'><img src="+item.images.large+" alt=''><span class='font-h'>"+item.title+"</span></a></div>";
-    };
-    Find.prototype.fetchData = function () {
+    Find.prototype.nextPage = function() {
+        this.page++;
         return dingeTools.movie({
             page: this.page
         });
     };
-    Find.prototype.makeData = function(result) {
-        const { getTemplate } = this;
-        let html = "";
-        if(result.status == 1 &&  result.data.list.length>0) {
-            let data = result.data.list;
-            data.forEach((item) => {
-                html += "<div class='swiper-slide'>"+getTemplate(item)+"</div>";
+    Find.prototype.getTemplate = function () {
+        return new Promise((resolve, reject) => {
+            this.nextPage()
+            .then((result) => {
+                let html = "";
+                if(result.status == 1 &&  result.data.list.length>0) {
+                    let data = result.data.list;
+                    data.forEach((item) => {
+                        html += "<div class='swiper-slide'><div class='search_item'><a href='moviedetails.html?id="+item._id+"'><img src="+item.images.large+" alt=''><span class='font-h'>"+item.title+"</span></a></div></div>";
+                    });
+                }
+                $(html).appendTo($(".swiper-wrapper"));
+                resolve();
+            }, (err) => {
+                reject(err);
             });
-            $(html).appendTo($(".swiper-wrapper"));
-        }
+        });
+        
     };
     Find.prototype.skipEvent = function () {
         $("#search").on("touchend", function() {
